d3488325c305cfacd2045da79cdd2081bc80459b
Abel González Rodríguez <agonzalez@altice.com.do>
2020-12-28 14:01:17 -0400

    add express

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+const express = require('express');
+const app = express();
 const scrapeServices = require("./services/scrapeServices");
 const readPdfServices = require("./services/readPdfServices");
 var cron = require('node-cron');
 require('dotenv').config();
  
+app.listen(process.env.PORT, () => {
+  console.log(`App listening at http://localhost:${process.env.PORT}`)
+  cron.schedule(process.env.cronRules, () => {
+    readPdfServices.deletePdfs();
+    console.log('Init scraping process');
+    scrapeServices.downloadPdfs();
+    setTimeout(()=>{
+      readPdfServices.insertPdfs();
+    },50000)
+    
+  });
+})
 
-console.log("start app");
 
-cron.schedule(process.env.cronRules, () => {
-  readPdfServices.deletePdfs();
-  console.log('Init scraping process');
-  scrapeServices.downloadPdfs();
-  setTimeout(()=>{
-    readPdfServices.insertPdfs();
-  },50000)
-  
-});
 
