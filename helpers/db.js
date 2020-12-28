25f6317d98ed208bf2af8ab3ed01cfe648067e68
Abel González Rodríguez <agonzalez@altice.com.do>
2020-12-28 00:56:44 -0400

    Add MONGODB_URI to enviroment variables

--- helpers/db.js
+++ helpers/db.js
@@ -1,7 +1,8 @@
 var mongoose = require("mongoose");
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost:27017/proconsumer', { useNewUrlParser: true },{ useUnifiedTopology: true });
 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true },{ useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
